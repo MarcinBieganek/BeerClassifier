4822300f07307517ce0442228658dc5bc4682425
MarcinBieganek <marcin.bieganek@gmail.com>
2024-06-14 17:01:26 +0200

    Html template for file upload added

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ config = load_config()
 
 @app.route('/get-beer-classification', methods=['GET', 'POST'])
 def get_beer_classification():
-    return make_response(jsonify({'message': 'You will be able to get beer classification here!'}), 200)
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
